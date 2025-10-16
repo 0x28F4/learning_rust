481f0f1c94fd71cb79e611abb3c2a80b29250919
0x28F4 <71667108+0x28F4@users.noreply.github.com>
2025-10-16 13:21:41 +0200

    proper solution

--- words/src/main.rs
+++ words/src/main.rs
@@ -1,9 +1,22 @@
 fn main() {
+    let mut s = "123";
+
+    s = "321";
     let words = String::from("Hello world!");
 
-    let first = &words[..5];
-    let second = &words[6..];
+    let first = first_word(&words);
 
-    println!("{first},{second}")
+    // this doesnt work anymore
+    // words.clear();
+    println!("{first}");
+}
+
+fn first_word(s: &str) -> &str {
+    for (i, &sb) in s.as_bytes().iter().enumerate() {
+        if sb == b' ' {
+            return &s[..i];
+        }
+    }
+    return &s[..];
 }
 
