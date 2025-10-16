943e4f6b69668fa170334e836471c42173fce133
0x28F4 <71667108+0x28F4@users.noreply.github.com>
2025-10-16 12:00:16 +0200

    out of bounds

--- words/src/main.rs
+++ words/src/main.rs
@@ -1,10 +1,12 @@
 fn main() {
-    let words = "Hello world!";
+    let mut words = String::from("Hello world!");
     let w = first_word_index(&words.to_string());
-    let (first, _) = words.split_at(w);
-    println!("{first}");
-}
+    words.clear();
 
+    // byte index 5 is out of bounds of ``
+    let (first, _) = words.split_at(w);
+    println!("{words} {first} {w}");
+}
 
 fn first_word_index(s: &String) -> usize {
     let bytes = s.as_bytes();
