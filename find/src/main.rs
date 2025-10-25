3ce1429e4a364a6e8b4fe7a0804b1a0a5e86fb22
0x28F4 <71667108+0x28F4@users.noreply.github.com>
2025-10-25 21:19:41 +0200

    find kinda works

--- find/src/main.rs
+++ find/src/main.rs
@@ -4,34 +4,21 @@ use std::path::Path;
 
 fn main() {
     let mut args = env::args().skip(1);
-    let query = args.next().unwrap();
-    println!("search for \"{query}\"");
-
-    let res = fs::read_dir(".").unwrap();
-    for item in res {
-        let item = item.unwrap();
-        let path_name = item.path();
-        let Some(path_name) = path_name.to_str() else {continue};
-        if path_name.contains(&query) {
-            println!("{path_name}");      
-        }
-    }
+    let path = args.next().unwrap();
+    println!("search for \"{path}\"");
+    let path = Path::new(&path);
+    find(path)
 }
 
-
-fn find(query: &str, p: &Path) {
+fn find(p: &Path) {
     let res = fs::read_dir(p).unwrap();
     for item in res {
         let item = item.unwrap();
         let path = item.path();
-
         if path.is_dir() {
-            find(&query, &path);
-            continue;
+            find(&path);
         }
         let Some(path_name) = path.to_str() else {continue};
-        if path_name.contains(&query) {
-            println!("{path_name}");      
-        }
+        println!("{path_name}");      
     }
 }
