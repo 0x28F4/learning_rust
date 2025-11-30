e346ad01c008533a26870860c0993423f77c503c
0x28F4 <71667108+0x28F4@users.noreply.github.com>
2025-11-30 12:23:20 +0100

    words

--- words/src/main.rs
+++ words/src/main.rs
@@ -1,7 +1,4 @@
 fn main() {
-    let mut s = "123";
-
-    s = "321";
     let words = String::from("Hello world!");
 
     let first = first_word(&words);
@@ -9,6 +6,15 @@ fn main() {
     // this doesnt work anymore
     // words.clear();
     println!("{first}");
+
+
+    let lhs = String::from("lhs");
+    let rhs = String::from("rhs");
+    let out = pick(&lhs, &rhs, true);
+
+    println!("{out}");
+
+    wtf();
 }
 
 fn first_word(s: &str) -> &str {
@@ -20,3 +26,19 @@ fn first_word(s: &str) -> &str {
     return &s[..];
 }
 
+fn pick<'a>(a: &'a str, b: &'a str, pick_a: bool) -> &'a str {
+    if pick_a {
+        return &a;
+    }
+    return b;
+}
+
+
+
+fn wtf() {
+    let v = String::from("1");
+    let longest = |b: & str| -> & str { &b[..] };
+
+    let l = longest(&v);
+    println!("{l}");
+}
